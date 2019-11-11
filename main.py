5c6b6ae41134010f1e737574c2d6ecd91d3e2af5
ahsanfayyaz74 <ahsan@wukla.com>
2019-11-11 19:29:39 +0500

    Added ortools

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import ortools
 
-
+st.sidebar.title("Parameters")
+st.sidebar.text("")
+st.sidebar.text("")
 
 st.title('Capacitated Vehicle Routing Problem')
 print("")
 st.header('Using docplex...')
+
+st.sidebar.text("Number of buses = 15")
+st.sidebar.text("")
+st.sidebar.slider("Number of students on every bus", 0, 100, 10)
+st.sidebar.text("")
+st.sidebar.text('Capacity of every bus = 100')
+st.sidebar.text("")
+
+if st.sidebar.button("Run Algorithm"):
+    st.sidebar.text("Some magic!")
